dde14aa14175351f01c2836388458d6a40c74cc2
Soumyajit <soumyajitdatta@gmail.com>
2024-07-10 23:41:41 +0530

    express websocket setting up(error) 10/7/24 1 hr 2 mins

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("hello world");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const serverConfig_1 = __importDefault(require("./config/serverConfig"));
+const http_1 = __importDefault(require("http"));
+const app = (0, express_1.default)();
+const server = http_1.default.createServer(app);
+server.listen(serverConfig_1.default.PORT, () => {
+    console.log(`Server is up at port ${serverConfig_1.default.PORT}`);
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world welcome bro");
+import express from "express";
+import ServerConfig from "./config/serverConfig";
+import http from "http";
+
+const app = express();
+
+const server = http.createServer(app);
+
+server.listen(ServerConfig.PORT, () => {
+    console.log(`Server is up at port ${ServerConfig.PORT}`);
+});
