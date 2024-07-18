bbb86e9fc873f06b0f341f3c570cff10b5c1f646
Soumyajit <soumyajitdatta@gmail.com>
2024-07-18 23:58:51 +0530

    room handlers on server site created, 26 mins, 18/7/24

---
+++ server/dist/handlers/roomHandler.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const roomHandler = (socket) => {
+    const createRoom = () => {
+    };
+};
+exports.default = roomHandler;
---
+++ server/src/handlers/roomHandler.ts
@@ -0,0 +1,19 @@
+import { Socket } from "socket.io";
+import {v4 as UUIDv4} from "uuid";
+const roomHandler = (socket: Socket) => {
+        const createRoom = ()=>{
+            const roomId=UUIDv4();//unique room id for multiple connection exchange
+                socket.join(roomId);//socket connection enters new room
+                socket.emit("room-created", { roomId});// emission of event from server site
+        };
+
+        const joinRoom = () => {
+            console.log("New room joined");
+        }
+
+
+        socket.on("create-room", createRoom);
+        socket.on("join-room", joinRoom);
+};
+
+export default roomHandler;
